430b6de35ca0f72bc3add4745527ce9d2f2d7600
曾玟凱 <zll12545@gmail.com>
2017-11-14 19:05:18 +0800

    關於我頁面增加profile區塊. 新增css特效

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -9,8 +9,27 @@ const navToggle = () => {
 	$('.navcontent').slideToggle("slow");
 };
 
-const loadContent = () => {
+const page = {
+	now:'repoTable'
+}
+
+const loadContent = (next) => {
 	if($(document).width() <= 600) {
 		navToggle();
+	}
+	if(page.now !== next){
+		navLight(event);
+		$('.'+page.now).slideToggle(200, function() {
+			$('.'+next).fadeToggle(400);
+		});
+		page.now = next;
+	}
+
+
+
+	function navLight(e) {
+		let a = document.querySelector('.navLight');
+		a.className = '';
+		e.target.className = 'navLight';
 	}
 };
